b2822fa56dccf656fa38eeff89e7207aff780b15
rosauro-hmesolutions <rosauro@hmesolutions.com.au>
2024-09-07 04:37:10 +0800

    chore: added maintenance-status page

--- src/Pages/MaintenanceStatus/index.tsx
+++ src/Pages/MaintenanceStatus/index.tsx
@@ -1,24 +1,61 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { Card, CardBody, Col, Container, Row } from "reactstrap";
 import Breadcrumb from "Components/Common/Breadcrumb";
+import "./maintenance-status.css";
+import MaintenanceStatusHeader from "./components/MaintenanceStatusHeader";
+import { DATA_VIEW_MODE } from "Components/constants/constants";
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { groupBy } from "lodash";
+import FleetGridView from "./components/FleetGridView";
+
+const FleetOrder = ["DUMP_TRUCK", "EXCAVATOR", "LOADER"];
 
 const MaintenanceStatus = (props: any) => {
-    document.title = "Maintenance Status | FMS Live";
+  document.title = "Maintenance Status | FMS Live";
 
-    return (
-        <React.Fragment>
-            <div className="page-content">
-                <Container fluid>
-                    <Breadcrumb title="Maintenance" breadcrumbItem="Maintenance Status" />
-                    <Row>
-                        <Col lg="12">
+  const [viewMode, setViewMode] = useState<DATA_VIEW_MODE>(
+    DATA_VIEW_MODE.TABLE
+  );
 
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        </React.Fragment >
-    )
-}
+  const selectProperties = createSelector(
+    (state: any) => state.Fleet,
+    (fleetState) => ({
+      fleetList: fleetState.data,
+      loading: fleetState.loading,
+    })
+  );
+
+  const { fleetList, loading } = useSelector(selectProperties);
+
+  const normalizedFleetList = useMemo(
+    () => groupBy(fleetList, (fleet) => fleet.category),
+    [fleetList]
+  );
+
+  return (
+    <React.Fragment>
+      <div className="page-content maintenance-status">
+        <Container fluid>
+          <Breadcrumb title="Maintenance" breadcrumbItem="Maintenance Status" />
+          <Row>
+            <Col lg="12">
+              <MaintenanceStatusHeader
+                viewMode={viewMode}
+                onChangeViewMode={setViewMode}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <FleetGridView
+              fleetOrder={FleetOrder}
+              fleetData={normalizedFleetList}
+            />
+          </Row>
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default MaintenanceStatus;
