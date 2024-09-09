66cfb8c137d27f5f9b62f62904fbe7b43c5ea67c
Night <hotcoding85@gmail.com>
2024-09-09 06:55:16 +0300

    change ROM Waste Summary Page Style

--- src/Pages/RomWasteSummary/components/RomStatusTableCard.tsx
+++ src/Pages/RomWasteSummary/components/RomStatusTableCard.tsx
@@ -1,6 +1,11 @@
 import _ from "lodash";
 import { Col, Row } from "reactstrap"
-
+import {
+    FLEET_TIME_STATE_COLOR,
+    LAYOUT_MODE_TYPES,
+  } from "Components/constants/layout";
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
 export const RomStatusTableCard = () => {
     const dataSource = [
         {
@@ -9,7 +14,8 @@ export const RomStatusTableCard = () => {
             grade: 1.9,
             ore_from_pit: 0,
             stock_to_crusher: -321.68,
-            stock: 23876.8
+            stock: 23876.8,
+            bgColor: '#FFB84C'
         },
         {
             key: '2',
@@ -17,7 +23,8 @@ export const RomStatusTableCard = () => {
             grade: 1.8,
             ore_from_pit: 562.01,
             stock_to_crusher: -121.68,
-            stock: 13876.8
+            stock: 13876.8,
+            bgColor: '#FFB84C'
         },
         {
             key: '3',
@@ -25,7 +32,8 @@ export const RomStatusTableCard = () => {
             grade: 1.7,
             ore_from_pit: 0,
             stock_to_crusher: -542.25,
-            stock: 0
+            stock: 0,
+            bgColor: '#FFB84C'
         },
         {
             key: '4',
@@ -33,7 +41,8 @@ export const RomStatusTableCard = () => {
             grade: 1.6,
             ore_from_pit: 652.12,
             stock_to_crusher: -142.78,
-            stock: 12852.6
+            stock: 12852.6,
+            bgColor: '#C58E42'
         },
         {
             key: '5',
@@ -41,7 +50,8 @@ export const RomStatusTableCard = () => {
             grade: 0.8,
             ore_from_pit: 932.24,
             stock_to_crusher: 0,
-            stock: 9845.2
+            stock: 9845.2,
+            bgColor: '#C58E42'
         },
         {
             key: '6',
@@ -49,7 +59,8 @@ export const RomStatusTableCard = () => {
             grade: 0.5,
             ore_from_pit: 458.67,
             stock_to_crusher: 0,
-            stock: 7452.7
+            stock: 7452.7,
+            bgColor: '#8F5D28'
         },
         {
             key: '7',
@@ -57,7 +68,8 @@ export const RomStatusTableCard = () => {
             grade: 0.3,
             ore_from_pit: 300.54,
             stock_to_crusher: -192.34,
-            stock: 0
+            stock: 0,
+            bgColor: '#5D3C17'
         },
     ];
       
@@ -88,11 +100,21 @@ export const RomStatusTableCard = () => {
             key: 'stock',
         },
     ];
+    const { layoutModeType } = useSelector(
+        createSelector(
+          (state: any) => state.Layout,
+          (layout) => ({
+            layoutModeType: layout.layoutModeTypes,
+          })
+        )
+    );
+    const isLight = layoutModeType === LAYOUT_MODE_TYPES.LIGHT;
+
     return (
         <>
             <Row>
                 <Col md={12}>
-                    <table className="table no-border" style={{}}>
+                    <table className={isLight ? 'table no-border light-mode' : 'table no-border dark-mode'} style={{}}>
                         <thead>
                             <tr>
                                 {_.map(columns, (col, index) => 
@@ -104,20 +126,20 @@ export const RomStatusTableCard = () => {
                             {_.map(dataSource, (col) => 
                                     <tr key={col.key}>
                                         <td style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                                            <div style={{width: '20px', height: '15px', background:'gold', marginRight: '10px'}}></div>
-                                            {col.location}
+                                            <div style={{width: '20px', height: '15px', background:col.bgColor, marginRight: '10px'}}></div>
+                                            <span className='text-color-normal'>{col.location}</span>
                                         </td>
                                         <td>
-                                            {col.grade.toLocaleString()}
-                                        </td>
-                                        <td style={{color: col.ore_from_pit >= 0 ? (col.ore_from_pit != 0 ? 'green' : '#a6b0cf') : 'red'}}>
-                                            {col.ore_from_pit.toLocaleString()}
-                                        </td>
-                                        <td style={{color: col.stock_to_crusher >= 0 ? (col.stock_to_crusher != 0 ? 'green' : '#a6b0cf') : 'red'}}>
-                                            {col.stock_to_crusher.toLocaleString()}
+                                            <span className='text-color-normal'>{col.grade.toLocaleString()}</span>
                                         </td>
                                         <td>
-                                            {col.stock.toLocaleString()}
+                                            <span className={col.ore_from_pit >= 0 ? (col.ore_from_pit != 0 ? 'text-color-green' : 'text-color-normal') : 'text-color-red'}>{col.ore_from_pit}</span>
+                                        </td>
+                                        <td>
+                                            <span className={col.stock_to_crusher >= 0 ? (col.stock_to_crusher != 0 ? 'text-color-green' : 'text-color-normal') : 'text-color-red'}>{col.stock_to_crusher.toLocaleString()}</span>
+                                        </td>
+                                        <td style={{textAlign: 'right'}}>
+                                            <span className="text-color-normal">{col.stock.toLocaleString()}</span>
                                         </td>
                                     </tr>
                             )}
