3f7632705eb1c7bee26f3180350fc06cc1a61966
rosauro-hmesolutions <rosauro@hmesolutions.com.au>
2024-09-24 17:21:45 +0800

    chore: updated reducers according to backend updates

--- src/slices/shiftroster/reducer.tsx
+++ src/slices/shiftroster/reducer.tsx
@@ -2,75 +2,80 @@ import { PayloadAction, Reducer, createSlice } from "@reduxjs/toolkit";
 import { User } from "slices/users/reducer";
 import { Vehicle } from "slices/fleet/reducer";
 
-
 interface CreateResponse {
-    code: number,
-    type: string,
-    success: boolean,
-    data: ShiftRoster,
+  code: number;
+  type: string;
+  success: boolean;
+  data: ShiftRoster;
 }
 
 interface ShiftRoster {
-    id: string,
-    roster: string,
-    operators: User[],
-    vehicle: Vehicle
+  id: string;
+  roster: string;
+  operators: User[];
+  vehicle: Vehicle;
 }
 
 export interface ShiftRosterState {
-    data: ShiftRoster[];
-    page: number;
-    limit: number;
-    total: number;
-    loading: boolean;
-    error: boolean | null;
-    errorMsg: string | null;
+  data: ShiftRoster[];
+  page: number;
+  limit: number;
+  total: number;
+  loading: boolean;
+  error: boolean | null;
+  errorMsg: string | null;
 }
 
 export const initialState: ShiftRosterState = {
-    data: [],
-    page: 1,
-    limit: 10,// for error msg
-    total: 0,
-    loading: false,
-    error: false,// for error
-    errorMsg: null
+  data: [],
+  page: 1,
+  limit: 10, // for error msg
+  total: 0,
+  loading: false,
+  error: false, // for error
+  errorMsg: null,
 };
 
 const shiftrosterSlice = createSlice({
-    name: "shiftroster",
-    initialState,
-    reducers: {
-        allSuccess(state, action) {
-            state.data = action.payload
-            state.total = action.payload.total
-            state.loading = false;
-            state.error = false;
-        },
-        createSuccess(state, action: PayloadAction<CreateResponse>) {
-            var newBench = action.payload.data
-            state.data = [...state.data, newBench]
-            state.loading = false;
-            state.error = false;
-        },
-        updateSuccess(state, action: PayloadAction<CreateResponse>) {
-            var newBench = action.payload.data
-            var data = state.data.filter(item => item.id !== newBench.id)
-            state.data = [...data, newBench]
-            state.loading = false;
-            state.error = false;
-        },
-        deleteSuccess(state, action) {
-            var deletedId = action.payload.data as string
-            state.data = state.data.filter(item => item.id !== deletedId);
-            state.loading = false;
-            state.error = false;
-        },
-        apiError(state, action) {
-            state.loading = true;
-            state.error = true;
-        },
-    }
+  name: "shiftroster",
+  initialState,
+  reducers: {
+    allSuccess(state, action) {
+      state.data = action.payload.results;
+      state.total = action.payload.totalResults;
+      state.loading = false;
+      state.error = false;
+    },
+    createSuccess(state, action: PayloadAction<CreateResponse>) {
+      var newBench = action.payload.data;
+      state.data = [...state.data, newBench];
+      state.loading = false;
+      state.error = false;
+    },
+    updateSuccess(state, action: PayloadAction<CreateResponse>) {
+      var newBench = action.payload.data;
+      var data = state.data.filter((item) => item.id !== newBench.id);
+      state.data = [...data, newBench];
+      state.loading = false;
+      state.error = false;
+    },
+    deleteSuccess(state, action) {
+      var deletedId = action.payload.data as string;
+      state.data = state.data.filter((item) => item.id !== deletedId);
+      state.loading = false;
+      state.error = false;
+    },
+    apiError(state, action) {
+      state.loading = true;
+      state.error = true;
+    },
+  },
 });
-export const { allSuccess, apiError, createSuccess, updateSuccess, deleteSuccess } = shiftrosterSlice.actions;
+export const {
+  allSuccess,
+  apiError,
+  createSuccess,
+  updateSuccess,
+  deleteSuccess,
+} = shiftrosterSlice.actions;
 export default shiftrosterSlice.reducer as Reducer<ShiftRosterState>;
--- src/slices/target/reducer.tsx
+++ src/slices/target/reducer.tsx
@@ -2,75 +2,80 @@ import { PayloadAction, Reducer, createSlice } from "@reduxjs/toolkit";
 import { User } from "slices/users/reducer";
 import { Vehicle } from "slices/fleet/reducer";
 
-
 interface CreateResponse {
-    code: number,
-    type: string,
-    success: boolean,
-    data: Target,
+  code: number;
+  type: string;
+  success: boolean;
+  data: Target;
 }
 
 interface Target {
-    id: string,
-    roster: string,
-    operators: User[],
-    vehicle: Vehicle
+  id: string;
+  roster: string;
+  operators: User[];
+  vehicle: Vehicle;
 }
 
 export interface TargetState {
-    data: Target[];
-    page: number;
-    limit: number;
-    total: number;
-    loading: boolean;
-    error: boolean | null;
-    errorMsg: string | null;
+  data: Target[];
+  page: number;
+  limit: number;
+  total: number;
+  loading: boolean;
+  error: boolean | null;
+  errorMsg: string | null;
 }
 
 export const initialState: TargetState = {
-    data: [],
-    page: 1,
-    limit: 10,// for error msg
-    total: 0,
-    loading: false,
-    error: false,// for error
-    errorMsg: null
+  data: [],
+  page: 1,
+  limit: 10, // for error msg
+  total: 0,
+  loading: false,
+  error: false, // for error
+  errorMsg: null,
 };
 
 const targetSlice = createSlice({
-    name: "target",
-    initialState,
-    reducers: {
-        allSuccess(state, action) {
-            state.data = action.payload
-            state.total = action.payload.total
-            state.loading = false;
-            state.error = false;
-        },
-        createSuccess(state, action: PayloadAction<CreateResponse>) {
-            var targets: any = action.payload.data;
-            state.data = targets;
-            state.loading = false;
-            state.error = false;
-        },
-        updateSuccess(state, action: PayloadAction<CreateResponse>) {
-            var newTarget = action.payload.data
-            var data = state.data.filter(item => item.id !== newTarget.id)
-            state.data = [...data, newTarget]
-            state.loading = false;
-            state.error = false;
-        },
-        deleteSuccess(state, action) {
-            var deletedId = action.payload.data as string
-            state.data = state.data.filter(item => item.id !== deletedId);
-            state.loading = false;
-            state.error = false;
-        },
-        apiError(state, action) {
-            state.loading = true;
-            state.error = true;
-        },
-    }
+  name: "target",
+  initialState,
+  reducers: {
+    allSuccess(state, action) {
+      state.data = action.payload.results;
+      state.total = action.payload.totalResults;
+      state.loading = false;
+      state.error = false;
+    },
+    createSuccess(state, action: PayloadAction<CreateResponse>) {
+      var targets: any = action.payload.data;
+      state.data = targets;
+      state.loading = false;
+      state.error = false;
+    },
+    updateSuccess(state, action: PayloadAction<CreateResponse>) {
+      var newTarget = action.payload.data;
+      var data = state.data.filter((item) => item.id !== newTarget.id);
+      state.data = [...data, newTarget];
+      state.loading = false;
+      state.error = false;
+    },
+    deleteSuccess(state, action) {
+      var deletedId = action.payload.data as string;
+      state.data = state.data.filter((item) => item.id !== deletedId);
+      state.loading = false;
+      state.error = false;
+    },
+    apiError(state, action) {
+      state.loading = true;
+      state.error = true;
+    },
+  },
 });
-export const { allSuccess, apiError, createSuccess, updateSuccess, deleteSuccess } = targetSlice.actions;
+export const {
+  allSuccess,
+  apiError,
+  createSuccess,
+  updateSuccess,
+  deleteSuccess,
+} = targetSlice.actions;
 export default targetSlice.reducer as Reducer<TargetState>;
