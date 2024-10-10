9a1752ae81901bce39e856417929ff1abd730d5c
Night <hotcoding85@gmail.com>
2024-10-10 09:22:38 +0300

    chore: update with 3D map component

--- src/Pages/HaulRoadOptimisation/index.tsx
+++ src/Pages/HaulRoadOptimisation/index.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { Col, Container, Row } from "reactstrap";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { Col, Container, Row, TabPane } from "reactstrap";
 import Breadcrumb from "Components/Common/Breadcrumb";
-import { Tabs, TabsProps } from "antd";
+import { Tabs, TabsProps, } from "antd";
 import HaulRoadOptimisationTableView from "./components/HaulRoadOptimisationTableView";
 import HaulRoadOptimisationMapView from "./components/HaulRoadOptimisationMapView";
 import HaulRoadOptimisationVisualView from "./components/HaulRoadOptimisationVisualView";
@@ -15,22 +15,19 @@ import JSZip from "@turbowarp/jszip";
 
 const HaulRoadOptimization = (props: any) => {
   document.title = "Haul Road Optimisation | FMS Live";
-  const [displayType, setDisplayType] = useState("TABLE");
-
-  const dispatch: any = useDispatch();
 
   const { vehicleRoutes } = useSelector(VehicleRouteSelector);
 
   const [replayRoads, setReplayRoad] = useState<any>([])
 
   const [geojsonData, setGeojsonData] = useState<any>(null)
-  const [imageData, setImageData] = useState<any>(null)
+
   useEffect(() => {
     const _roads = vehicleRoutes.filter(_route => _route.category !== 'STOP_SIGNS' && _route.status == 'ACTIVE')
     const _replayRoads: any = []
-    _.map(_roads, road => {
+    _.map(_roads, (road, index) => {
       const temp = {
-        label: road.name,
+        label: "Trip " + (index + 1),
         value: road.id
       }
       _replayRoads.push(temp)
@@ -41,79 +38,10 @@ const HaulRoadOptimization = (props: any) => {
   const [currentRoad, setCurrentRoad] = useState<DropdownType>({
     label: "Choose Replay Road",
   });
-  const tabItems: TabsProps["items"] = [
-    {
-      key: "table",
-      label: "Table View",
-    },
-    {
-      key: "map",
-      label: "Map View",
-    },
-    {
-      key: "visual",
-      label: "Visual Analytics",
-    },
-  ];
-  const onTabChange = (key: string) => {
-    if (key === "table") {
-      setDisplayType("TABLE");
-    } else if (key === "map") {
-      setDisplayType("MAP");
-    } else {
-      setDisplayType("VISUAL");
-    }
-  };
-
-  useEffect(() => {
-    fetchZipFile()
-  }, [])
-
-  const fetchZipFile = async () => {
-    const zipBuffer = await fetch('./240817_Pits_3D_WGS84.zip').then(response => response.arrayBuffer())
-    JSZip.loadAsync(zipBuffer).then(data => {
-        return data.file('240817_Pits_3D_WGS84.geojson')?.async("string");
-    }).then((text) => {
-        var _geojsonData = JSON.parse(text as string)
-        setGeojsonData(_geojsonData)
-        processZipFile(_geojsonData)
-    })
-  }
-
-  let animationFrameId: number;
-  let map: any;
-  const processZipFile = async (geojsonData) => {
-    // Fetch the ZIP file and get its ArrayBuffer
-    const zipBuffer = await fetch('./images.zip').then(response => response.arrayBuffer());
-    
-    // Initialize an object to hold image data
-    const _imageData = {};
-    
-    // Load the ZIP file using JSZip
-    const zip = await JSZip.loadAsync(zipBuffer);
-
-    // Create an array to hold promises
-    const promises: any = [];
-
-    // Iterate through each file in the ZIP
-    zip.forEach((relativePath, file) => {
-        // Check if the file is a WebP image
-        if (file.name.endsWith('.webp')) {
-            // Create a promise for each image processing
-            const promise = file.async('arraybuffer').then(data => {
-                // Extract the filename without extension
-                const fileNameWithoutExtension = file.name.replace(/\.[^/.]+$/, "");
-                // Store the image data in the object
-                _imageData[fileNameWithoutExtension] = data;
-            });
-            promises.push(promise);
-        }
-    });
-
-    // Wait for all promises to resolve
-    await Promise.all(promises);
-    setImageData(_imageData)
-  }
+  const [activeTab, setActiveTab] = useState<string>('1');
+  const onChangeTap = useCallback((key) => {
+    setActiveTab(key)
+  }, [activeTab])
 
   return (
     <React.Fragment>
@@ -129,12 +57,27 @@ const HaulRoadOptimization = (props: any) => {
               <Tabs
                 className="haul-raod-optimisation-tabs"
                 defaultActiveKey="1"
-                items={tabItems}
-                onChange={onTabChange}
-              />
+                // items={tabItems}
+                activeKey={activeTab}
+                onChange={(key) => onChangeTap(key)}
+              >
+                <TabPane tab="Table View" key="1">
+                  <HaulRoadOptimisationTableView />
+                </TabPane>
 
-              
-              <div className="replay-road-dropdown" style={{position: 'absolute', top: '0px', right: '12px', display: displayType === "MAP" ? 'block' : 'none'}}>
+                <TabPane tab="Table View" key="2">
+                  <HaulRoadOptimisationMapView
+                    geojsonData={geojsonData}
+                    currentRoad={currentRoad}
+                    replayRoads={vehicleRoutes.filter(_route => _route.category !== 'STOP_SIGNS' && _route.status == 'ACTIVE')}
+                  />
+                </TabPane>
+
+                <TabPane tab="Visual Analytics" key="3">
+                  <HaulRoadOptimisationVisualView />
+                </TabPane>
+              </Tabs>
+              <div className="replay-road-dropdown" style={{position: 'absolute', top: '0px', right: '12px', display: activeTab == "2" ? 'block' : 'none'}}>
                 <Dropdown
                   label="Choose Replay"
                   items={replayRoads}
@@ -145,13 +88,6 @@ const HaulRoadOptimization = (props: any) => {
             </Col>
           </Row>
 
-          {displayType === "TABLE" ? (
-            <HaulRoadOptimisationTableView />
-          ) : displayType === "MAP" ? (
-            <HaulRoadOptimisationMapView geojsonData={geojsonData} imageData={imageData} currentRoad={currentRoad} replayRoads={vehicleRoutes.filter(_route => _route.category !== 'STOP_SIGNS' && _route.status == 'ACTIVE')} />
-          ) : (
-            <HaulRoadOptimisationVisualView />
-          )}
         </Container>
       </div>
     </React.Fragment>
