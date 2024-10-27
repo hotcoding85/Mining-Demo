be7f078a04f29c19d5e23809b86a98bb769a2dfb
Pavlo <pavlo@hmesolutions.com.au>
2024-10-27 18:23:49 +0200

    chore: modify initial layout of the gantt scheduler

--- src/Pages/GanttScheduler/index.tsx
+++ src/Pages/GanttScheduler/index.tsx
@@ -16,10 +16,20 @@ import Breadcrumb from "Components/Common/Breadcrumb";
 import { ShiftType, Plan, resourceHeight } from "./interfaces/type";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { DatePicker, DatePickerProps, Space, Badge } from "antd";
+import { DatePicker, DatePickerProps, Space, Avatar, Tooltip, Badge } from "antd";
 import dayjs from "dayjs";
 import "./styles/GanttScheduler.scss";
 import "../../App.css";
+import {
+  hd1500,
+  hd785,
+  pc1250,
+  pc2000,
+  placeHolder,
+  wa600,
+  d375,
+  t45,
+} from "assets/images/equipment";
 import {
   getAllBenches,
   getAllFleet,
@@ -338,6 +348,38 @@ const GanttScheduler: React.FC = () => {
     setZoomSize(nearestZoom);
   };
 
+  function containsCaseInsensitive(str: string, substr: string): boolean {
+    return str.toLowerCase().includes(substr.toLowerCase());
+  }
+
+  const getImage = (category: string) => {
+    if (!category) {
+      return placeHolder;
+    }
+
+    if (containsCaseInsensitive(category, "hd785")) {
+      return hd785;
+    } else if (containsCaseInsensitive(category, "hd1500")) {
+      return hd1500;
+    } else if (containsCaseInsensitive(category, "pc1250")) {
+      return pc1250;
+    } else if (containsCaseInsensitive(category, "pc2000")) {
+      return pc2000;
+    } else if (containsCaseInsensitive(category, "wa600")) {
+      return wa600;
+    } else if (containsCaseInsensitive(category, "d375")) {
+      return d375;
+    } else if (containsCaseInsensitive(category, "t45")) {
+      return t45;
+    } else {
+      return placeHolder;
+    }
+  };
+
+  useEffect(() => {
+    console.log(plans)
+  },[ plans ])
+
   return (
     <React.Fragment>
       <div className="page-content" style={{paddingBottom: '0rem'}}>
@@ -390,6 +432,47 @@ const GanttScheduler: React.FC = () => {
                         openModal={openModal}
                       />
                     </Card>
+                    <div className="row mt-2">
+                      {excavators.map((equipment) => {
+                        // Filter out all plans for the specific equipment
+                        const equipmentPlans = plans.filter(plan => plan.excavatorId === equipment.id);
+                        
+                        // Calculate total duration in milliseconds
+                        const totalDurationMs = equipmentPlans.reduce((total, plan) => {
+                          const start = new Date(plan.startTime).getTime();
+                          const end = new Date(plan.endTime).getTime();
+                          return total + (end - start);
+                        }, 0);
+
+                        // Convert milliseconds to hours and minutes
+                        const hours = Math.floor(totalDurationMs / (1000 * 60 * 60));
+                        const minutes = Math.floor((totalDurationMs % (1000 * 60 * 60)) / (1000 * 60));
+
+                        // Format as hh:mm
+                        const duration = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+                        return (
+                          <Col sm={6} xs={6} lg={4} key={equipment.id}>
+                            <Card className="p-4 flex items-center space-x-4 aggregate-item">
+                              <div style={{ flexDirection: 'row', display: 'flex', alignItems: 'center' }}>
+                                <Avatar style={{ backgroundColor: '#00000080' }} size={48}>
+                                  <img style={{ width: '30px', height: '30px' }} src={getImage(equipment.model)} alt="equipment" />
+                                </Avatar>
+                                <div className="flex-grow ml-1" style={{marginLeft: '5px'}}>
+                                  <h4 className="font-bold" style={{ marginBottom: '0px' }}>{equipment.name}</h4>
+                                  <h5 style={{marginBottom: '0px'}}>{duration}</h5> 
+                                </div>
+                              </div>
+                              <Tooltip title={equipment.state}>
+                                <Badge 
+                                  status={equipment.state === 'ACTIVE' ? 'success' : equipment.state !== 'STANDBY' ? 'error' : 'warning'} 
+                                  className="aggregate-item-badge" 
+                                />
+                              </Tooltip>
+                            </Card>
+                          </Col>
+                        );
+                      })}
+                    </div>
                   </Col>
                   <Col md={3} sm={4} xs={12} className="plan-right">
                     <Badge.Ribbon placement="start" text="NO TIME ASSIGNED" color="Orange" style={{fontWeight: 600, fontSize: '14px'}}>
